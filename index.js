839130b31b871889191d9933a42b0a6a8efb4990
sakmongkhon018 <aucss02@gmail.com>
2022-07-18 17:24:41 +0700

    express

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')//1.Use the express module
+const app = express()             //2.Create an object of the express module
+
+const PORT = process.env.PORT || 5000 //3.Setting port
+const hostname = "localhost"
+
+//4.Create Route: //http://localhost:5000/
+app.get('/',(req,res)=>{
+    //res.status(200).send("Express")
+    res.status(200).json({
+         id:'101',
+        user:'Mark',
+        level:'amdin' 
+    })
+})
+
+app.listen(PORT,()=>{ //5.Make the server
+    console.log(`Server running at http://${hostname}:${PORT}`)
+})
